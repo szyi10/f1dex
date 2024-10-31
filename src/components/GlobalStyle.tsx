f498f3d18f86a422fb8e6d7669a7a5b1f24a3616
Szymon Kędzior <szymon.ked16@gmail.com>
2024-10-31 14:57:03 +0100

    Navbar added

--- src/components/GlobalStyle.tsx
+++ src/components/GlobalStyle.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-// import Navbar from "./layout/Navbar";
+import Navbar from "./layout/navbar/Navbar";
 
 const GlobalStyle = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
-      {/* <Navbar /> */}
+      <Navbar />
       {children}
       <p className="text-center my-2 text-neutral-400">
         2024 &copy; szyi - v.1.0.0
--- src/components/layout/Navbar.tsx
+++
@@ -1,9 +0,0 @@
-const Navbar = () => {
-  return (
-    <header className="max-w-screen-xl w-full mx-auto mt-8 px-3">
-      <div className="w-full bg-neutral-50 h-20 rounded-xl shadow-[rgba(17,_17,_26,_0.1)_0px_0px_16px]"></div>
-    </header>
-  );
-};
-
-export default Navbar;
