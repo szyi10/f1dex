8926aa801007f015dffa6a741cfa09a495cb83d8
szyi <szymon.ked16@gmail.com>
2024-12-26 13:39:19 +0100

    feat: added drivers handlers

--- backend/handlers/drivers.go
+++ backend/handlers/drivers.go
@@ -19,12 +19,23 @@ import (
 func HandleAllDrivers(c *fiber.Ctx) error {
 	coll := database.GetCollection("drivers")
 
+	// Query params
+	sortBy := c.Query("sortBy", "name")
+
+	order := c.Query("order", "asc")
+
 	filter := bson.M{}
+
 	opts := options.Find().SetSkip(0).SetLimit(30)
+	if order == "asc" {
+		opts.SetSort(bson.D{{Key: sortBy, Value: 1}})
+	} else {
+		opts.SetSort(bson.D{{Key: sortBy, Value: -1}})
+	}
 
 	cursor, err := coll.Find(c.Context(), filter, opts)
 	if err != nil {
-		return c.Status(500).JSON(fiber.Map{"internal server error": err.Error()})
+		return c.Status(500).JSON(fiber.Map{"error": err.Error()})
 	}
 
 	drivers := make([]models.Driver, 0)
@@ -41,6 +52,8 @@ func HandleAllDrivers(c *fiber.Ctx) error {
 // @Accept */*
 // @Produce json
 // @Param id path string true "Driver ID"
+// @Param sortBy query string false "Sort by" Enums(name, currentSeason.pointScored)
+// @Param order query string false "Order" Enums(asc, desc)
 // @Success 200 {object} models.Driver
 // @Router /drivers/{id} [get]
 func HandleDriverByID(c *fiber.Ctx) error {
@@ -61,4 +74,72 @@ func HandleDriverByID(c *fiber.Ctx) error {
 	}
 
 	return c.Status(200).JSON(driver)
+}
+
+// @Summary Get driver by slug name.
+// @Description fetch a driver by their slug name.
+// @Tags drivers
+// @Accept */*
+// @Produce json
+// @Param slug path string true "Driver Slug Name"
+// @Success 200 {object} models.Driver
+// @Router /drivers/slug/{slug} [get]
+func HandleDriverBySlugName(c *fiber.Ctx) error {
+	slug := c.Params("slug")
+
+	coll := database.GetCollection("drivers")
+	filter := bson.M{"slug": slug}
+	var driver models.Driver
+
+	err := coll.FindOne(c.Context(), filter).Decode(&driver)
+	if err != nil {
+		return c.Status(404).JSON(fiber.Map{"error": err.Error()})
+	}
+
+	return c.Status(200).JSON(driver)
+}
+
+// @Summary Search drivers.
+// @Description search drivers by name
+// @Tags drivers
+// @Accept */*
+// @Produce json
+// @Param query query string true "Search query"
+// @Param sortBy query string false "Sort by" Enums(name, currentSeason.pointScored)
+// @Param order query string false "Order" Enums(asc, desc)
+// @Success 200 {object} []models.Driver
+// @Router /drivers/search [get]
+func HandleSearchDrivers(c *fiber.Ctx) error {
+	coll := database.GetCollection("drivers")
+
+	query := c.Query("query")
+	sortBy := c.Query("sortBy", "currentSeason.pointScored")
+	order := c.Query("order", "desc")
+
+	filter := bson.M{
+		"$or": []bson.M{
+			{"name": bson.M{"$regex": query, "$options": "i"}},
+			{"nationality": bson.M{"$regex": query, "$options": "i"}},
+			{"team": bson.M{"$regex": query, "$options": "i"}},
+		},
+	}
+
+	opts := options.Find().SetSkip(0).SetLimit(30)
+	if order == "asc" {
+		opts.SetSort(bson.D{{Key: sortBy, Value: 1}})
+	} else {
+		opts.SetSort(bson.D{{Key: sortBy, Value: -1}})
+	}
+
+	cursor, err := coll.Find(c.Context(), filter, opts)
+	if err != nil {
+		return c.Status(500).JSON(fiber.Map{"internal server error": err.Error()})
+	}
+
+	drivers := make([]models.Driver, 0)
+	if err = cursor.All(c.Context(), &drivers); err != nil {
+		return c.Status(500).JSON(fiber.Map{"internal server error": err.Error()})
+	}
+
+	return c.Status(200).JSON(drivers)
 }
