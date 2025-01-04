fcc5a7a06b9905033245f3f64cea9d5c576c3954
szyi <szymon.ked16@gmail.com>
2025-01-04 21:02:30 +0100

    feat: added homepage for dashbaord

--- backend/web/static/js/main.js
+++ backend/web/static/js/main.js
@@ -1,3 +1,26 @@
 window.addEventListener("DOMContentLoaded", () => {
-  console.log("JS Loaded");
+  // Update 'Server Status' homepage element
+  checkStatus().then((res) => {
+    document.getElementById(
+      "server-status"
+    ).innerText = `${res.text} - ${res.code}`;
+  });
 });
+
+async function checkStatus() {
+  try {
+    const res = await fetch("/api/v1/health");
+
+    return {
+      code: res.status,
+      text: res.statusText,
+    };
+  } catch (error) {
+    console.log(error);
+
+    return {
+      code: 0,
+      text: "CLIENT ERROR",
+    };
+  }
+}
